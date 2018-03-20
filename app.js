d0877f6d50e9286e8fdaafeb6c36f99758cd2a4a
nussam <sam@nustechnology.com>
2018-03-20 13:38:29 +0700

    Partially collapse sidebar

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-$(document).ready(function () {
-  $('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
-});
+// $(function () {
+//   // whenever we hover over a menu item that has a submenu
+//   $('body').on('mouseover', '#sidebar.active li#page-list', function () {
+//     console.log('gogo');
+
+//     var $menuItem = $(this),
+//     $submenuWrapper = $('> .wrapper', $menuItem);
+
+//     // grab the menu item's position relative to its positioned parent
+//     var menuItemPos = $menuItem.position();
+
+//     // place the submenu in the correct position relevant to the menu item
+//     $submenuWrapper.css({
+//       top: menuItemPos.top,
+//       left: menuItemPos.left + Math.round($menuItem.outerWidth() * 0.75)
+//     });
+//   });
+// });
